24c066e8f9fb3752fefece2f14d9df9ca3bf8ed6
fa091263 <fa091263@C39384.ens.uqam.ca>
2017-02-09 21:11:02 -0500

    modif

--- src/laboratoireeagil/Laboratoireeagil.java
+++ src/laboratoireeagil/Laboratoireeagil.java
@@ -1,4 +1,4 @@
-/*
+/*agile c bien 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,6 +15,7 @@ public class Laboratoireeagil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
     }
     
